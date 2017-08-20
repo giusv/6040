2589f26ac4bbfe64068667a335e9681f90dc7399
Giuseppe Viola <gius.viola@gmail.com>
2017-08-20 15:41:21 +0200

    test

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 10000000
+double x[N],y[N];
+int main(){
+    int i;
+    double alpha = 0.1;
+    double one_minus_alpha = 1.0 - alpha;
+    for(i=0;i<N;i++) {
+        x[i] = 1.0;
+        y[i] = 0.0;
+    }
+
+    y[0] = alpha*x[0];
+
+    for(i=1;i<N;i++) {
+        y[i] = alpha*x[i] + one_minus_alpha*y[i-1];
+    }
+//    for(i=0;i<N;i++) {
+//        printf("%f %f\n", x[i], y[i]);
+//    }
+//
+    return 0;
+}
